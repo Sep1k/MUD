73a0a10536acdcdf91a01bc4adf081af6bc7f26c
Sep1k <richard.ritso@voco.ee>
2025-02-12 11:58:17 +0200

    Update client.py
    katse 1

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
-SERVER_IP = '192.168.1.112'  # Muuda see serveri tegelikuks IP-aadressiks
+SERVER_IP = '172.20.2.26'  # Muuda see serveri tegelikuks IP-aadressiks
 PORT = 12345
 
 while True:
@@ -15,6 +15,12 @@ while True:
         client_socket.connect((SERVER_IP, PORT))
         client_socket.sendall(message.encode('utf-8'))
         print("Sõnum saadetud!")
+
+        # Oodake serveri vastust
+        data = client_socket.recv(1024)  # Loe kuni 1024 baiti
+        if data:  # Kontrollime, et andmed pole tühjad
+            print(f"Vastus serverilt: {data.decode('utf-8')}")
+        
     except Exception as e:
         print(f"Tekkis viga: {e}")
     finally:
