303357195a3a5f812e4754feb172ae360ac68156
Sep1k <richard.ritso@voco.ee>
2025-02-12 10:28:36 +0000

    working servers now
    sever answers back

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ server_socket.listen(5)  # Lubab kuni 5 ühendust järjekorras
 
 print(f"Server kuulab {HOST}:{PORT}")
 
+queston = "hello"
+Mkorrad = 0
 while True:
     client_socket, addr = server_socket.accept()
     print(f"Ühendus: {addr}")
@@ -17,8 +19,13 @@ while True:
     data = client_socket.recv(1024)  # Loe kuni 1024 baiti
     if data:  # Kontrollime, et andmed pole tühjad
         print(f"Sõnum: {data.decode('utf-8')}")
-        if data.decode('utf-8') == "tere":
+        question = data.decode('utf-8')
+        if question == "tere":
             client_socket.sendall("tere".encode('utf-8'))
+        elif question == "map":
+            Mkorrad += 1
+            client_socket.sendall(str("Map has been looked at " + str(Mkorrad))map.encode('utf-8'))
 
     client_socket.close()
+
 
