728c0cbfe71d9bbfa642958a5605ab4830d4798b
Sep1k <richard.ritso@voco.ee>
2025-02-13 07:58:05 +0000

    Update server.py
    moved game funktions into def

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = '0.0.0.0'  # Kuulab kõiki võrguadaptereid
-PORT = 12345      # Port, mida kuulatakse
+PORT = 12345  # Port, mida kuulatakse
 
 # Loo socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,8 +10,20 @@ server_socket.listen(5)  # Lubab kuni 5 ühendust järjekorras
 
 print(f"Server kuulab {HOST}:{PORT}")
 
-queston = "hello"
+q = "hello"
 Mkorrad = 0
+
+def Game_brain():
+    global q, Mkorrad
+    q = q.lower()
+    if q == "tere" or q == "hi" or q == "hello" or q == "you":
+        return "Tere"
+    elif q == "map":
+        Mkorrad += 1
+        return "Mappi on vaadatud " + str(Mkorrad)
+    else:
+        return "Vigane command"
+
 while True:
     client_socket, addr = server_socket.accept()
     print(f"Ühendus: {addr}")
@@ -19,13 +31,9 @@ while True:
     data = client_socket.recv(1024)  # Loe kuni 1024 baiti
     if data:  # Kontrollime, et andmed pole tühjad
         print(f"Sõnum: {data.decode('utf-8')}")
-        question = data.decode('utf-8')
-        if question == "tere":
-            client_socket.sendall("tere".encode('utf-8'))
-        elif question == "map":
-            Mkorrad += 1
-            client_socket.sendall(str("Map has been looked at " + str(Mkorrad))map.encode('utf-8'))
+        q = data.decode('utf-8')
+        vaste = Game_brain()
+        client_socket.sendall(vaste.encode('utf-8'))
 
     client_socket.close()
-
 
