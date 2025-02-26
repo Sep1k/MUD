a933500333c8fc8072a61cedc83add5d1f987f7f
Sep1k <richard.ritso@voco.ee>
2025-02-26 13:20:24 +0000

    Update start.py

--- start.py
+++ start.py
@@ -1,19 +1,29 @@
 import winsound
 from tkinter import *
+import socket
+import subprocess
 
-
+hostname = socket.gethostname()
+ip =  socket.gethostbyname(hostname)
+port = ""
+print(ip)
 try:  # Laulu mängimine igavesti
     while True:
-        winsound.PlaySound('Ambient.wav', winsound.SND_FILENAME)
+        winsound.PlaySound('filaes/Ambient.wav', winsound.SND_FILENAME)
 except:
     pass
 
+gamestate = 0 #mainmenu
+           #2 host menu
+           #3 join screen
+           #4 ready to game
+           
 def Game():
-    asd #siia tuleb gameluup. hetkel client nime all
+    pass #siia tuleb gameluup. hetkel client nime all
 root = Tk()  # Akna tegemine
 
 root.title("MUD Chickantis")
-icon = PhotoImage(file='game icon.png')
+icon = PhotoImage(file='filaes/game icon.png')
 root.iconphoto(True, icon)
 
 # Scrollbar
@@ -70,18 +80,70 @@ root.bind_all("<MouseWheel>", on_mouse_wheel)
 # Kui hiirt vajutatakse, siis muutub aktiivseks tekstikast.
 def on_click(event):
     input_text.focus()
-
+    
 root.bind("<Button-1>", on_click)
 
 last_data = ""  
 new_data = ""
-
+joined_ip = ""
+joined_port = ""
 # Enteri peale salvestatakse kirjutuskasti sisu ja tehakse see tühjaks
+
 def capture_enter(event):
-    global last_data, logo_art, root
+    global last_data, logo_art, root, ip, port, gamestate, joined_ip, joined_port
+    print(gamestate)
     current_data = input_text.get("1.0", END).strip() # võtab kirjutatud lõigu
+    if gamestate == 4:
+        joined_port == current_data
+        print(joined_port)
+    if gamestate == 3:
+        joined_ip == current_data
+        logo_text.config(state=NORMAL)
+        logo_text.delete("1.0", END) 
+        logo_art = (str(joined_ip + "\nEnter port you want to join"))
+        logo_text.insert(END, logo_art)
+        logo_text.config(state=DISABLED)
+        logo_text.see(END)
+        gamestate = 4
     
-    
+    if gamestate == 2:
+        port = current_data
+        try:
+            if int(port) < 10000 and int(port) > 1000:
+                print("port kontrollitud")
+                port = current_data
+                
+                print("kirjutan faili")
+                with open('filaes/kaluriped.txt', 'w') as file: 
+                    pass #millegipärast kui fail kirjutamiseks avada ja sinna mitte midagi kirjutada kustutatakse selle sisu.
+                with open('filaes/kaluriped.txt', 'w') as file:
+                    file.write(ip + "\n" + port)
+                print("avan uue programmi")
+                try:
+                    subprocess.run(['python', 'filaes/server.py'])
+                except:
+                    pass
+                print("programm avatud")
+                gamestate = 4
+                
+
+            else:
+                logo_text.config(state=NORMAL)  
+                logo_text.delete(1.0, END)
+                logo_art += "\nPort is invalid. \nTry again."
+                logo_text.insert(END, logo_art) 
+                logo_text.config(state=DISABLED)
+                input_text.delete("1.0", END)
+                return
+        except:
+            logo_text.config(state=NORMAL)  
+            logo_text.delete(1.0, END)
+            logo_art += "\nPort is invalid. \nTry again."
+            logo_text.insert(END, logo_art) 
+            logo_text.config(state=DISABLED)
+            input_text.delete("1.0", END)
+            return
+        
     if current_data:
         print("New data captured:", current_data)
         
@@ -96,14 +158,37 @@ def capture_enter(event):
 
         
         if "help" in current_data:
-            print("saadi kätte abikutse")
+            print("Help command received")
+
+            input_text.delete("1.0", END)
+            help_text = "1. To join a game, type 'join' and press enter.\n2. To host the game, type 'host' and press enter."
+            logo_art += "\n" + help_text
+            logo_text.config(state=NORMAL)
+            logo_text.insert(END, "\n" + help_text) 
+            logo_text.config(state=DISABLED)
+            logo_text.see(END)
         if "host" in current_data:
-            #host funktion here
-            asd
+            print("User tryed hosting.")
+            
+            logo_text.config(state=NORMAL)
+            logo_text.delete("1.0", END) 
+            logo_art = (str("HOSTING GAME\n IP = " + ip + "\nType in port for game to host on(4 didget number):"))
+            logo_text.insert(END, logo_art)
+            logo_text.config(state=DISABLED)
+            logo_text.see(END)
+            gamestate = 2 
         if "join" in current_data:
-            #join funktion here
-            asd
-    current_data = ""
+            print("user tryed to join")
+            logo_text.config(state=NORMAL)
+            logo_text.delete("1.0", END) 
+            logo_art = (str("JOINING GAME\nEnter ip address to you want to join:"))
+            logo_text.insert(END, logo_art)
+            logo_text.config(state=DISABLED)
+            logo_text.see(END)
+            gamestate = 3
+            
+        else:
+            input_text.delete("1.0", END)
 input_text.bind("<Return>", capture_enter)
 
 mainloop()
